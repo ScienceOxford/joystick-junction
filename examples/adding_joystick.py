41ff1d8a4bbb4d7d3055099ddc26cad0e3e5f6d0
Sarah Townson <26084137+Isioviel@users.noreply.github.com>
2019-09-06 12:09:55 +0100

    Create adding_joystick.py

---
+++ examples/adding_joystick.py
@@ -0,0 +1,26 @@
+from microbit import *
+from joystick import button_press, joystick_push
+from game import road
+
+# Please tag us if used!
+# We'd love to see what you make:
+# @ScienceOxford
+
+x = 0
+y = 0
+b = 9
+
+while True:
+    sleep(150)
+    road()
+    display.set_pixel(x, y, b)
+    joystick = joystick_push()
+    button = button_press()
+    if joystick[1] < 516 and y < 4:
+        y += 1
+    if joystick[1] > 536 and y > 0:
+        y -= 1
+    if joystick[0] > 519 and x < 4:
+        x += 1
+    if joystick[0] < 499 and x > 0:
+        x -= 1
